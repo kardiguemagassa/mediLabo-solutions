fba15253067c4759f7b81e0feb4f0bd6d2d2c8bb
kardiguemagassa <129184661+kardiguemagassa@users.noreply.github.com>
2026-03-06 17:23:26 +0100

    add package-lock.json, package.json, src/app/component/authorizationserver/register/register.component.ts

--- frontend/mediLabo-solutions-ui/src/app/component/authorizationserver/register/register.component.ts
+++ frontend/mediLabo-solutions-ui/src/app/component/authorizationserver/register/register.component.ts
@@ -1,7 +1,11 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { StorageService } from '../../../service/storage.service';
 import { UserService } from '../../../service/user.service';
+import { NgForm } from '@angular/forms';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { HotToastService } from '@ngxpert/hot-toast';
+
 
 
 @Component({
@@ -13,12 +17,31 @@ import { UserService } from '../../../service/user.service';
 export class RegisterComponent {
   state = signal<{loading: boolean, message: string, error: string | any}>({ loading: false, message: undefined, error: undefined});
   private router = inject(Router);
+  private destroyRef = inject(DestroyRef);
   private storage = inject(StorageService);
   private userService = inject(UserService);
+  private toastService = inject(HotToastService);
 
   ngOnInit() : void {
     if (this.userService.isAuthenticated() && this.userService.isTokenExpired()) {
       this.storage.getRedirectUrl() ? this.router.navigate([this.storage.getRedirectUrl()]) : this.router.navigate(['/dashboard']);
     }
+  }
+
+  closeMessage = () => this.state.set({loading: false, message: undefined, error: undefined});
+
+  register = (form: NgForm) => {
+    this.state.set({loading: true, message: undefined, error: undefined});
+    this.userService.register$(form.value).pipe(takeUntilDestroyed(this.destroyRef)).subscribe({
+      next: (response) => {
+        this.state.set({loading: false, message: response.message, error: undefined});
+        this.toastService.success(response.message);
+      },
+      error: (error: string) => {
+        this.state.set({loading: false, message: undefined, error});
+        this.toastService.error(error);
+      },
+      complete: () => form.reset()
+    });
   }
 }
