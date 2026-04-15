0c68ab5bda59e9dd721b6f2c9b29415b0ab44ef3
kardiguemagassa <129184661+kardiguemagassa@users.noreply.github.com>
2026-04-15 11:56:06 +0200

    backend/assessmentservice/src/test/java/com/openclassrooms/assessmentservice/service/implementation/PatientServiceClientImplTest.java

--- backend/assessmentservice/src/test/java/com/openclassrooms/assessmentservice/service/implementation/PatientServiceClientImplTest.java
+++ backend/assessmentservice/src/test/java/com/openclassrooms/assessmentservice/service/implementation/PatientServiceClientImplTest.java
@@ -295,7 +295,7 @@ class PatientServiceClientImplTest {
 
     @Test
     @DisplayName("Should handle response with missing fields")
-    @Timeout(value = 30, unit = TimeUnit.SECONDS)
+    //@Timeout(value = 30, unit = TimeUnit.SECONDS)
     void getPatientByUuid_MissingFields() {
 
         String mockJsonResponse = """
